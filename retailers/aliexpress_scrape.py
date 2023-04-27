ccd0657d34ce410b5c17d42a2a8fb98714d7fa5d
rafael <bboyrafinhazika@gmail.com>
2023-04-27 00:34:14 -0300

    fix some details

--- retailers/aliexpress_scrape.py
+++ retailers/aliexpress_scrape.py
@@ -47,13 +47,13 @@ class AliExpress:
                 if price_info[i]['skuVal']['availQuantity'] == 0:
                     return -1, store
         price_float_value = float(price[3:].replace(',', ''))
-        full_price = price_float_value
         try:
             slogan_banner = data['middleBannerModule']['uniformMiddleBanner']['sloganBanner']
             if slogan_banner == 'Preço exclusivo na primeira compra':
                 price_float_value += 16
         except: 
             pass
+        full_price = price_float_value
         promo_desc = 0
         try:
             promo_15_off_a_cada = data['couponModule']['webCouponInfo']['promotionPanelDTO']['acrossStoreFixedDiscount'][0]['promotionPanelDetailDTOList'][0]['promotionDesc'].replace('(', '').replace(')', '')
@@ -90,16 +90,15 @@ class AliExpress:
                 all_discounts = all_discounts_list[i]['promotionDetailList']
                 if 'Tenha' in all_discounts[0]:
                     for discount in all_discounts:
-                        shop_discounts.append(float(discount[discount.find('$') + 2:discount.find(',') + 3].replace('.', '').replace(',', '.')))
-                        shop_discounts_conditions.append(float(discount[discount.rfind('$') + 2: discount.rfind(',') + 3].replace('.', '').replace(',', '.')))
+                        shop_discounts.append(float(discount[discount.find('$') + 2:discount.find('.') + 3]))
+                        shop_discounts_conditions.append(float(discount[discount.rfind('$') + 2: discount.rfind('.') + 3]))
                 if 'Compre 1 ' in all_discounts[0]:
                     percent_value = [float(x) for x in all_discounts[0][all_discounts[0].find('1') + 1:all_discounts[0].find('%')].split(' ') if x.isdigit()][0]
                     shop_percent_discount_off = round(price_float_value*percent_value/100, 2)
-                    print(shop_percent_discount_off)
             shop_discounts.sort()
             shop_discounts_conditions.sort()
         except Exception as e:
-            pass
+            print(e)
         price_float_value -= shop_percent_discount_off    
         shop_discount_off = 0
         for i in range(len(shop_discounts)):
@@ -135,4 +134,4 @@ class AliExpress:
 
 if __name__ == '__main__':
     aliexpress = AliExpress()
-    print(aliexpress.scrape('https://pt.aliexpress.com/item/1005003807598286.html?spm=a2g0o.store_pc_groupList.8148356.37.4930494bnKaqSC&pdp_npi=2%40dis%21BRL%21R%24%20593%2C13%21R%24%20189%2C82%21R%24%20189%2C82%21%21%21%21%402101e9d116817006179147639e51a1%2112000032207898711%21sh', sku = '12000032207898712'))
+    print(aliexpress.scrape('https://s.click.aliexpress.com/e/_DBSBp25', sku = '12000032033893862'))
