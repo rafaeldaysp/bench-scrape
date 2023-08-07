3f4a3b343e49295e8598cd6f00e11ac16343c175
rafael <bboyrafinhazika@gmail.com>
2023-08-07 14:11:31 -0300

    commit changes

--- retailers/avell_scrape.py
+++ retailers/avell_scrape.py
@@ -6,6 +6,13 @@ class Avell:
     def __init__(self) -> None:
         self.retailer_id = 'd95b62cd-78c0-465f-8d6e-80151c3c567e'
     def coupon_validation(self, description, product):
+        if description:
+            try:
+                #description = json.loads(description)
+                if product['id'] not in description:
+                    return False
+            except Exception as e:
+                pass
         return True
     
     def bestCashbackFinder(self):
@@ -32,4 +39,6 @@ class Avell:
         return price, 'Avell'
 
 if __name__ == '__main__':
-    pass
+    avell = Avell()
+    price = avell.scrape('https://avell.com.br/avell-storm-two')
+    print(price)
