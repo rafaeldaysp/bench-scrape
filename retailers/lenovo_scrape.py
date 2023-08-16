64e76c15187c640fa1dfa890f751bd399db394b2
rafael <bboyrafinhazika@gmail.com>
2023-08-16 15:17:57 -0300

    commit changes

--- retailers/lenovo_scrape.py
+++ retailers/lenovo_scrape.py
@@ -3,7 +3,8 @@ import requests
 from fake_useragent import UserAgent
 from dotenv import load_dotenv
 import os
-from lxml import etree
+import json
+from api import api
 
 load_dotenv()
 
@@ -29,6 +30,13 @@ class Lenovo:
 ]
 
     def coupon_validation(self, description, product):
+        if description:
+            try:
+                if description == 'CASHBACKNOTALLOWED': return True 
+                if product['id'] not in description:
+                    return False
+            except:
+                pass
         return True
 
     def get_response(self, url):
@@ -42,12 +50,9 @@ class Lenovo:
             for cashbackProvider in self.cashbackProviders:
                 r = self.get_response(cashbackProvider["url"])
                 soup = BeautifulSoup(r.content, "html.parser")
-
-                dom = etree.HTML(str(soup))
                 cashbackFullLabelArray = soup.find('span', class_='rewardsTag-cashback').text.split(
                     " "
                 )
-
                 for label in cashbackFullLabelArray:
                     if "%" in label:
                         cashbackProvider["value"] = float(
@@ -62,22 +67,57 @@ class Lenovo:
         return cashback
 
     def scrape(self, url, **kwargs):
+        sku = kwargs['sku']
+        product_id = kwargs['product_id']
+        base_url = f'https://www.lenovo.com/br/pt/p/{sku}/singlev2/price/json'
+        price = None
         
-        price = -2
-        site = BeautifulSoup(self.get_response(url).content, 'html.parser')
+        coupons = api.get_coupons(self.retailer_id)
+        
         try:
-            price = float(site.find('dd', {'itemprop': 'price'}).text[3:].replace('.', '').replace(',', '.'))
+            site = BeautifulSoup(self.get_response(url).content, 'html.parser')
             stock_msgs = site.findAll('span', {'class': 'stock_message'})
             count = 0
             for msg in stock_msgs:
                 if msg.text == 'Esgotado':
                     count += 1
             if count == len(stock_msgs):
-                price = -1
-        except Exception as e:
-            price = -2
-            print(e)
+                return -1, 'Lenovo'
+            data = json.loads(self.get_response(base_url).content)
+            price = float(data['potentialDiscountedPrice'].replace('.',  '').replace(',', '.'))
+            coupon_code = data['eCoupon']
+            if (coupon_code):
+                discount = str(int(price - float(data['potentialCouponPrice'].replace('.',  '').replace(',', '.'))))
+                if not any([coupon['discount'] == discount and coupon['code'].strip() == coupon_code and product_id in coupon['description'] for coupon in coupons]):
+                    formated_code = coupon_code
+                    while any([coupon['code'] == formated_code for coupon in coupons]):
+                        formated_code = formated_code + ' '
+                    print(f'O cupom {coupon_code} será criado.')
+                    r = api.create_coupon({
+                        "available": True,
+                        "code": formated_code,
+                        "discount": discount,
+                        "retailer_id": self.retailer_id,
+                        "description": product_id
+                    })
+                    
+        except:
+            pass
+        
+        # try:
+        #     price = float(site.find('dd', {'itemprop': 'price'}).text[3:].replace('.', '').replace(',', '.'))
+        #     stock_msgs = site.findAll('span', {'class': 'stock_message'})
+        #     count = 0
+        #     for msg in stock_msgs:
+        #         if msg.text == 'Esgotado':
+        #             count += 1
+        #     if count == len(stock_msgs):
+        #         price = -1
+        # except Exception as e:
+        #     price = -2
+        #     print(e)
+        print('Preço: ', price)
         return price, 'Lenovo'
 
 if __name__ == '__main__':
-    print(Lenovo().scrape(url, product_id='d1dce1af-14d4-4e9c-b588-cc809e20d8c0'))
+    print(Lenovo().scrape(url, product_id='d1dce1af-14d4-4e9c-b588-cc809e20d8c0', sku='82TB0000BR'))
